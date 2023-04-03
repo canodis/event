033fa245b4add8a2bd3a500cfddf666104c5870e
Rafet can Tosun <rtosun@k2m10s01.42kocaeli.com.tr>
2023-04-03 20:02:40 +0300

    can

--- ex14/sastantua.cpp
+++ ex14/sastantua.cpp
@@ -2,23 +2,17 @@
 
 using namespace std;
 
-
 void    put_empty(int level, int q)
 {
-    int width = 1;
-    int step = 4;
-    int i = 1;
+    int base = 7;
+	int inc = 12;
 
-    while (i <= level)
-    {
-        width += 2 * (2 + i);
-        i++;
-        width += step;
-        if (i % 2 && i < level)
-            step += 2;
-    }
-    width -= step;
-    for (int i = 0; i < (width - q) / 2; i++)
+	for (int increments = 1; increments < level; increments++)
+	{
+		base += inc;
+		inc += (increments % 2 == 0 ? 4 : 2);
+	}
+    for (int i = 0; i < (base - q) / 2; i++)
         printf(" ");
 }
 
@@ -34,8 +28,7 @@ void    put_line(int width)
             printf("*");
     }
 }
-//+6 +7 +9 +10 +12 +13 +15
-//2 8 15 24 34 46 59 74
+
 void sastantua(int level)
 {
     int floor = 1;
@@ -60,12 +53,4 @@ int main(int ac, char **av)
 {
     sastantua(av[1][0] - 48);
 }
-
-//3 5 7  
-//13 15 17 19
-//25
-//41
-
-// 1 -> 7    2 -> 19    3 -> 33       4 -> 51      5 -> 71
-//         + 12        +14           +18        +20
 
